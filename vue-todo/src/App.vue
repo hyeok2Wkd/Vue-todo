6e1610c82317f2b1f2c195a07a4d3b86fc62c2ba
Black Rabbit <newyork@Blackui-iMac-5.local>
2023-02-27 17:59:34 +0900

    TodoList컴포넌트의 할 일 완료 기능 구현

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoFooter from "./components/TodoFooter.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+
+export default {
+  name: 'App',
+  components: {
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
+  },
+  created(){
+    console.log("hello");
+  }
+}
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #F6F6F8;
+}
+
+</style>
---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <ul>
+        <li v-for="todoItem, index in todoItems" :key="index" class="shadow">
+            <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted : todoItem.completed}" v-on:click="toogleComplete(todoItem, index)"></i>
+            <span v-bind:class="{textCompleted : todoItem.completed}">{{ todoItem.item }}</span>
+            <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+                <i class="fas fa-trash-alt"></i>
+            </span>
+        </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+    data: function(){
+        return{
+            todoItems: [],
+        }
+    },
+    created:function(){
+        if(localStorage.length > 0){
+            for(var i = 0;i<localStorage.length;i++){
+                // console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
+                this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+            }
+        }
+    },
+    methods:{
+        removeTodo:function (todoItem, index) {
+            console.log(todoItem, index);
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index, 1);
+        },
+        toogleComplete:function (todoItem, index) {
+            todoItem.completed = !todoItem.completed;
+            localStorage.removeItem(todoItem.item);
+            localStorage.setItem(todoItem.item, JSON.stringify(todoItem))
+            console.log(index);
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+
+
+</style>
